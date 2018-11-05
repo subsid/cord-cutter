f4a59632e7d58dc5f022dd6bf244e3364552958c
Siddharth <sidharth3930@gmail.com>
2018-11-05 12:32:48 -0600

    Add has_belongs_to_many assoc for channels and packages

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_004041) do
+ActiveRecord::Schema.define(version: 2018_11_05_175602) do
 
   create_table "channels", force: :cascade do |t|
     t.string "name"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "channels_stream_packages_joins", id: false, force: :cascade do |t|
+    t.integer "channel_id"
+    t.integer "stream_package_id"
   end
 
   create_table "stream_packages", force: :cascade do |t|
