abf714992b59209a2ff1c2a371ac67619caaab9e
Siddharth <sidharth3930@gmail.com>
2018-11-05 13:52:41 -0600

    Add channels scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'stream_packages#index'
   resources :stream_packages
+  resources :channels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
