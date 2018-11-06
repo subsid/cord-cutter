c02a8c9f402da56442b7cae2ee1b9a5c99dbe8ae
EC2 Default User <ec2-user@ip-172-31-34-249.us-west-2.compute.internal>
2018-11-06 04:28:19 +0000

    User Channels

---
+++ app/models/channel.rb
@@ -0,0 +1,3 @@
+class Channel < ApplicationRecord
+    has_many :channels_user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_002910) do
+ActiveRecord::Schema.define(version: 2018_11_03_205200) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "channels_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "channel_id"
+    t.string "preferences"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
